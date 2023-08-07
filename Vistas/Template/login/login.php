9436d164d6fa67701d7a13f2a4938c9fc16c35eb
Isma1398 <ismael.guillermo@hotmail.com>
2023-08-07 11:00:47 -0600

    Login

---
+++ Vistas/Template/login/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="/Recursos/Css/login.css">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+
+<body>
+    <div class="login-container">
+        <div class="login-card">
+            <div class="logo-container">
+                <img src="/Img/MGC_Logo_EngraneB.png" alt="Logo" class="logo">
+            </div>
+            <div class="input-group">
+                <span class="icon"><i class="fa fa-user"></i></span>
+                <input type="text" placeholder="Usuario">
+            </div>
+            <div class="input-group">
+                <span class="icon"><i class="fa fa-lock"></i></span>
+                <input type="password" placeholder="Contraseña">
+            </div>
+            <button>Entrar</button>
+        </div>
+    </div>
+</body>
+
+</html>
